e88ab9c97e1445f5820a58ec6485db78c3e5512d
Sashko Stubailo <sashko@meteor.com>
2018-07-16 13:59:12 -0700

    Add more

--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -3,10 +3,14 @@ const _ = require("lodash");
 
 const API = "https://dog.ceo/api";
 
-async function getDisplayImage(dog) {
-  const results = await fetch(`${API}/breed/${breed}/images/random`);
-  const { message: image } = await results.json();
-  return image;
+async function getDisplayImage(breed) {
+  try {
+    const results = await fetch(`${API}/breed/${breed}/images/random`);
+    const { message: image } = await results.json();
+    return image;
+  } catch (e) {
+    // lol
+  }
 }
 
 exports.handler = async (event, context, callback) => {
@@ -22,6 +26,14 @@ exports.handler = async (event, context, callback) => {
   recordTime("first request received");
   const { message: dogs } = await results.json();
   recordTime("first request parsed");
+
+  const imagePromises = Object.keys(dogs).map(async breed => {
+    const image = await getDisplayImage(breed);
+  });
+
+  recordTime("made all promises");
+  const result = await Promise.all(imagePromises);
+  recordTime("fetched more dogs");
 
   callback(null, {
     body: JSON.stringify(times),
