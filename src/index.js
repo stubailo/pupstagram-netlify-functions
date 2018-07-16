31701ba9c65c2717b274172fe56ce66205cf3ee2
Sashko Stubailo <sashko@meteor.com>
2018-07-16 13:49:03 -0700

    timing

--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
-import React from "react";
-import { AppRegistry, View } from "react-native-web";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-
-import Feed from "./views/Feed";
-import Detail from "./views/Detail";
-import Likes from "./views/Likes";
-import { defaults, resolvers } from "./resolvers";
-
-const client = new ApolloClient({
-  uri: "/.netlify/functions/graphql",
-  clientState: {
-    defaults,
-    resolvers
-  }
-});
-
-const App = () => (
-  <ApolloProvider client={client}>
-    <Router>
-      <View>
-        <Switch>
-          <Route path="/" exact component={Feed} />
-          <Route path="/likes" exact component={Likes} />
-          <Route path="/:breed/:id" component={Detail} />
-        </Switch>
-      </View>
-    </Router>
-  </ApolloProvider>
-);
-
-AppRegistry.registerComponent("App", () => App);
-AppRegistry.runApplication("App", { rootTag: document.getElementById("root") });
+fetch("/.netlify/functions/graphql")
+  .then(res => res.json())
+  .then(res => {
+    document.body.innerHTML = `
+    <pre>${JSON.stringify(res, null, 2)}</pre>
+  `;
+  });
--- src/lambda/graphql.js
+++ src/lambda/graphql.js
@@ -1,82 +1,30 @@
-const { ApolloServer, gql } = require("apollo-server-lambda");
 const fetch = require("node-fetch").default;
-const { unique } = require("shorthash");
 const _ = require("lodash");
-
-console.log(fetch);
 
 const API = "https://dog.ceo/api";
 
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Query {
-    dogs: [Dog]
-    dog(breed: String!): Dog
+async function getDisplayImage(dog) {
+  const results = await fetch(`${API}/breed/${breed}/images/random`);
+  const { message: image } = await results.json();
+  return image;
+}
+
+exports.handler = async (event, context, callback) => {
+  const times = [];
+  const startTime = +new Date();
+
+  function recordTime(name) {
+    times.push({ name, time: +new Date() - startTime });
   }
 
-  type Dog {
-    id: String!
-    breed: String!
-    displayImage: String
-    images: [Image]
-    subbreeds: [String]
-  }
+  recordTime("start");
+  const results = await fetch(`${API}/breeds/list/all`);
+  recordTime("first request received");
+  const { message: dogs } = await results.json();
+  recordTime("first request parsed");
 
-  type Image {
-    url: String!
-    id: String!
-  }
-`;
-
-const createDog = (subbreeds, breed) => ({
-  breed,
-  id: unique(breed),
-  subbreeds: subbreeds.length > 0 ? subbreeds : null
-});
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    dogs: async () => {
-      const results = await fetch(`${API}/breeds/list/all`);
-      const { message: dogs } = await results.json();
-
-      return _.map(dogs, createDog);
-    },
-    dog: async (root, { breed }) => {
-      const results = await fetch(`${API}/breed/${breed}/list`);
-      const { message: subbreeds } = await results.json();
-
-      return createDog(subbreeds, breed);
-    }
-  },
-  Dog: {
-    displayImage: async ({ breed }) => {
-      const results = await fetch(`${API}/breed/${breed}/images/random`);
-      const { message: image } = await results.json();
-      return image;
-    },
-    images: async ({ breed }) => {
-      const results = await fetch(`${API}/breed/${breed}/images`);
-      const { message: images } = await results.json();
-      return images.map(image => ({ url: image, id: unique(image) }));
-    }
-  }
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-});
-
-const asHandler = server.createHandler({
-  cors: {
-    origin: "*",
-    credentials: true
-  }
-});
-
-exports.handler = (...args) => {
-  console.log(args[0]);
-  return asHandler(...args);
+  callback(null, {
+    body: JSON.stringify(times),
+    statusCode: 200
+  });
 };
